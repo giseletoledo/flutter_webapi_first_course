16cfd1e098b10b85cf968e8724d10d682ed82d55
Gisele <gisesonia@gmail.com>
2022-12-13 12:25:25 -0300

    requisicao e resposta http com logger

--- lib/services/http_interceptors.dart
+++ lib/services/http_interceptors.dart
@@ -6,6 +6,28 @@ class LoggingInterceptor implements InterceptorContract {
 
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
+    logger.v(
+        "Requisição para ${data.baseUrl}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    if (data.statusCode ~/ 100 == 2) {
+      logger.i(
+          "Resposta de ${data.url}\nStatus da Resposta:${data.statusCode}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
+    } else {
+      logger.e(
+          "Resposta de ${data.url}\nStatus da Resposta:${data.statusCode}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
+    }
+
+    return data;
+  }
+
+  /* 
+  Sem logger
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
     print(data.toString());
     return data;
   }
@@ -14,5 +36,5 @@ class LoggingInterceptor implements InterceptorContract {
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
     print(data.toString());
     return data;
-  }
+  } */
 }
