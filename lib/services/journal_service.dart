55ea52bd3a93f21ad82b76afd13393251c2e2f50
Gisele <gisesonia@gmail.com>
2022-12-13 12:07:50 -0300

    visualizando informacoes sobre a requisicao http

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,9 +1,31 @@
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/intercepted_client.dart';
 
 class JournalService {
   static const String url = "http://192.168.0.13:3000/";
   static const String resource = "learnhttp/";
 
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+
+  String getUrl() {
+    return "$url$resource";
+  }
+
+  register(String content) {
+    client.post(Uri.parse(getUrl()), body: {"content": content});
+  }
+
+  Future<String> get() async {
+    http.Response response = await client.get(Uri.parse(getUrl()));
+    print(response.body);
+    return response.body;
+  }
+
+  /* 
+  Sem http interceptor
+  
   String getUrl() {
     return "$url$resource";
   }
@@ -16,5 +38,5 @@ class JournalService {
     http.Response response = await http.get(Uri.parse(getUrl()));
     print(response.body);
     return response.body;
-  }
+  }*/
 }
