9fc59ebc36f4631112418594198a85e92c28c0b9
Gisele <gisesonia@gmail.com>
2022-12-13 14:07:56 -0300

    Teal de adicionar textos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/home_screen/add_journal_screen/add_jounal_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
@@ -19,18 +21,31 @@ class MyApp extends StatelessWidget {
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.grey,
-          appBarTheme: const AppBarTheme(
-            elevation: 0,
-            backgroundColor: Colors.black,
-            titleTextStyle: TextStyle(color: Colors.white),
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
           ),
-          textTheme: GoogleFonts.bitterTextTheme()),
+          actionsIconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
-      initialRoute: "home",
+      initialRoute: "add-journal",
       routes: {
         "home": (context) => const HomeScreen(),
+        "add-journal": (context) => AddJournalScreen(
+              journal: Journal(
+                  id: "id",
+                  content: "content",
+                  createdAt: DateTime.now(),
+                  updatedAt: DateTime.now()),
+            ),
       },
     );
   }
