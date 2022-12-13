7427fd50b0223dd10172d8f44a388c51f5de83f7
Gisele <gisesonia@gmail.com>
2022-12-13 12:08:21 -0300

    Adicionando o package logger

---
+++ lib/services/http_interceptors.dart
@@ -0,0 +1,18 @@
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  Logger logger = Logger();
+
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print(data.toString());
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print(data.toString());
+    return data;
+  }
+}
